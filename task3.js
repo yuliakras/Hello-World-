fac6b19ec349a512ca2b0cd16d1ba9bece8673a9
a@a.a <etenzor@gmail.com>
2018-07-23 21:11:48 +0300

    modify task3.js

--- task3.js
+++ task3.js
@@ -1,22 +1,28 @@
 btn.addEventListener('click', function(e) {
-    
-  var str1 = first.value;
   e.preventDefault();
-  var str2 = second.value;
-  
-  result.innerHTML = str2;
-
-
+  var str1 = first.value;
+var str2 = second.value;
 var strUser = sign.options[sign.selectedIndex].text;
+
+
+
 let expected =0;
 switch(strUser){
-    case "test2": alert(str1 + " " + str2 + " " + strUser); break;
-    case "+": expected = Number(str1) + Number(str2); break;
-    case "-": expected = Number(str1) - Number(str2); break;
-    case "*": expected = Number(str1) * Number(str2); break;
-    case "/": expected = Number(str1) / Number(str2); break;
-
+  case "test2": alert(str1 + " " + str2 + " " + strUser); break;
+  case "+": expected = Number(str1) + Number(str2); break;
+  case "-": expected = Number(str1) - Number(str2); break;
+  case "*": expected = Number(str1) * Number(str2); break;
+  case "/": expected = Number(str1) / Number(str2); break;
 }
+result.innerHTML = expected;
 
-alert(expected);
+// expected - real result;
 });
+function showMessage() {
+  if(str2 === excpected) {
+    result.innerHTML = 'Well done!';
+  } else if(str2 != expected) {
+    result.innerHTML = 'Try again.';
+  }
+  alert(expected);
+} 
